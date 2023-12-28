75cdd4468925c260e84f303c524fdeffb6e00b69
vdevigere <vdevigere+git@gmail.com>
2023-12-27 19:43:51 -0800

    Adjusting names and packages

--- activations/src/main/scala/org/novi/dsl/activations/FalseActivationConfig.scala
+++
@@ -1,7 +0,0 @@
-package org.novi.dsl.activations
-
-import org.novi.dsl.core.BaseActivationConfig
-
-class FalseActivationConfig(configuration: String) extends BaseActivationConfig(configuration) {
-  override def apply(context: String): java.lang.Boolean = false
-}
--- activations/src/main/scala/org/novi/dsl/activations/TrueActivationConfig.scala
+++
@@ -1,7 +0,0 @@
-package org.novi.dsl.activations
-
-import org.novi.dsl.core.BaseActivationConfig
-
-class TrueActivationConfig(configuration: String) extends BaseActivationConfig(configuration) {
-  override def apply(context: String): java.lang.Boolean = true
-}
